4d55b8abd02713c9b09470cf9916881eb32edd22
Roberval Filho <rslfilho@gmail.com>
2021-06-21 18:23:42 -0300

    Exercício 2

--- bloco_11/dia_1/src/App.js
+++ bloco_11/dia_1/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tasks = ['limpar a casa', 'fazer comida', 'completar exercícios'];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+        { tasks.map((task) => Task(task)) }
+        </ul>
       </header>
     </div>
   );
