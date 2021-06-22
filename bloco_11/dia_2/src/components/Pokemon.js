fffc553fbaddb7ad4796fe98338cf29cd1432433
Roberval Filho <rslfilho@gmail.com>
2021-06-22 19:51:30 -0300

    Add PropTypes

--- bloco_11/dia_2/src/components/Pokemon.js
+++ bloco_11/dia_2/src/components/Pokemon.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Pokemon extends Component {
   render(){
@@ -17,6 +18,18 @@ class Pokemon extends Component {
       </div>
     );
   };
+};
+
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      value: PropTypes.number,
+      measurementUnit: PropTypes.string,
+    }),
+    image: PropTypes.string,
+  })
 };
 
 export default Pokemon;
