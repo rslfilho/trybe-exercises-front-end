3f61afb86ade4889f0eac634083a875598723d30
Roberval Filho <rslfilho@gmail.com>
2021-06-24 18:53:53 -0300

    Criando componente PokeButton

--- bloco_12/dia_1/src/App.js
+++ bloco_12/dia_1/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Pokedex from './components/Pokedex';
-
+import PokeButton from './components/PokeButtons';
+import Pokemon from './components/Pokemon';
+import data from './data';
 
 function App() {
   return (
     <main>
       <h1>Pokedex</h1>
-      <Pokedex />
+      <Pokemon pokemon={data[0]}/>
+      <PokeButton />
     </main>
   );
 }
---
+++ bloco_12/dia_1/src/components/PokeButtons.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+class PokeButton extends Component {
+  render() {
+    return (
+      <section className="pokebutton">
+        
+      </section>
+    );
+  }
+}
+
+export default PokeButton;
