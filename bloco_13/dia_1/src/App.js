36158506a800a97597a46c5f2629eef7bdbd64c7
Roberval Filho <rslfilho@gmail.com>
2021-06-30 19:54:55 -0300

    Commit bomba pq eu esqueci kkkk

--- bloco_13/dia_1/src/App.js
+++ bloco_13/dia_1/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Dog from './components/Dog'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App
+        <Dog />
       </div>
     );
   }
